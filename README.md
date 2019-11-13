8dbefd4d2eca42c45385dcdfc77cad4d9284065d
tm-roamer <tm165822@163.com>
2019-11-13 21:00:46 +0800

    edit

---
+++ README.md
@@ -0,0 +1,5 @@
+## python-learn
+
+    python大法好, 趁着没啥事捅咕捅咕.
+
+    本文档记录日常学习python的各种示例和笔记.
---
+++ src/study/hello_world.py
@@ -0,0 +1,17 @@
+# 
+print('hello world')
+#
+print('hello world', 'jumps over')
+#
+print('100 + 200 =', 100 + 200)
+#
+#name = input()
+#print('your name:', name)
+#
+print('1024 * 768 =', 1024 * 768)
+#
+a = 100
+if a >= 0:
+  print(a)
+else:
+  print(-a)
