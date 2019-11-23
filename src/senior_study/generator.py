d4ae7f09fc3dbd6608284292acdbeb18b4186e9a
tm-roamer <tm165822@163.com>
2019-11-23 21:41:19 +0800

    edit

--- src/senior_study/generator.py
+++ src/senior_study/generator.py
@@ -38,6 +38,7 @@
 # next(o)
 # next(o)
 
+
 def fib(max):
     n, a, b = 0, 0, 1
     while n < max:
@@ -46,14 +47,15 @@ def fib(max):
         n = n + 1
     return 'done'
 
+
 # for n in fib(6):
 #     print(n)
 
 g = fib(6)
 while True:
-     try:
-         x = next(g)
-         print('g:', x)
-     except StopIteration as e:
-         print('Generator return value:', e.value)
-         break
+    try:
+        x = next(g)
+        print('g:', x)
+    except StopIteration as e:
+        print('Generator return value:', e.value)
+        break
